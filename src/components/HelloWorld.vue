50b90e223ffdd949a17b9069978c86672682ab14
Tom Grossi <TheAutomaTom@gmail.com>
2024-06-18 11:38:26 -0500

    Stub out elements

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -4,35 +4,33 @@ import { ref } from 'vue'
 defineProps<{ msg: string }>()
 
 const count = ref(0)
+
 </script>
 
 <template>
   <h1>{{ msg }}</h1>
 
   <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
-    </p>
+    
+    <hr/> <!-- =========================== -->
+    <h2>Input</h2>
+    <h3>
+      Username: <input/>      
+    </h3>
+    <h3 style="padding-bottom:2em;">
+      Message: <input/>
+    </h3>
+    
+    <hr/> <!-- =========================== -->
+    <h2>Output</h2>
+    <p>...</p>
+
+
+
+
   </div>
 
-  <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank"
-      >create-vue</a
-    >, the official Vue + Vite starter
-  </p>
-  <p>
-    Install
-    <a href="https://github.com/vuejs/language-tools" target="_blank">Volar</a>
-    in your IDE for a better DX
-  </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
 </template>
 
 <style scoped>
-.read-the-docs {
-  color: #888;
-}
 </style>
