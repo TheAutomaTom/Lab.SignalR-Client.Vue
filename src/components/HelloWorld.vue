70c0e5530d9c33d690907fcfcd220fcac6127bf3
Tom Grossi <TheAutomaTom@gmail.com>
2024-06-20 17:30:32 -0500

    wip

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,9 +1,27 @@
 <script setup lang="ts">
 import { ref } from 'vue'
+import * as signalR from '@microsoft/signalr';
 
 defineProps<{ msg: string }>()
 
-const count = ref(0)
+const connection = new signalR.HubConnectionBuilder()
+  .withUrl("/lab-hub")
+  .build();
+
+connection.start().catch(err => console.error(err.toString()));
+
+const Sender = ref("TheAutomaTom");
+const Outbound= ref("Can you hear me, now?");
+const Conversation= ref(["...", "...", "..."]);
+
+const Send =()=> {
+  connection.invoke("SendMessage", User.value, Outbound.value)
+    .catch(err => console.error(err.toString()));
+}
+
+connection.on("ReceiveMessage", (user, message) => {
+  Conversation.value = [...Conversation.value, `${user}:${message}`]
+});
 
 </script>
 
@@ -15,19 +33,32 @@ const count = ref(0)
     <hr/> <!-- =========================== -->
     <h2>Input</h2>
     <h3>
-      Username: <input/>      
+      Username: 
+      <input
+        v-model="Sender"
+      />
     </h3>
-    <h3 style="padding-bottom:2em;">
-      Message: <input/>
+    <h3>
+      Message: 
+      <input
+      v-model="Outbound"
+      />
     </h3>
+    <button
+      style="margin-bottom:2em; width:100%"
+      @Click="Send()"
+    >Send</button>
     
     <hr/> <!-- =========================== -->
     <h2>Output</h2>
-    <p>...</p>
+    <p
+      v-for="(entry, index) in Conversation"
+      :key="index"
+    >{{entry}}</p>
 
 
 
-
+    
   </div>
 
 </template>
